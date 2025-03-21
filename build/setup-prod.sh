06eb346282683385ded3deee4cd4a3445eddd709
Shiva Rajendran Viji (Trianz) <shivarajendran.viji@trianz.com>
2025-03-21 17:10:05 +0530

    update on setup-prod

--- build/setup-prod.sh
+++ build/setup-prod.sh
@@ -1,8 +1,9 @@
 # Get the private key from the environment variable
 echo "Setting up Production Connection..."
-mkdir keys
-echo $SFDC_SERVER_KEY | base64 -di > keys/server.key
+mkdir -p keys  # Create keys directory if it doesn't exist
+echo $SFDC_SERVER_KEY | base64 --decode > keys/server.key
+chmod 600 keys/server.key
 
 # Authenticate to salesforce
 echo "Authenticating..."
-sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile keys/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a DevHub
+sfdx force:auth:jwt:grant -i $SFDC_PROD_CLIENTID -f keys/server.key -u $SFDC_PROD_USER --set-default-dev-hub -a devhub3
