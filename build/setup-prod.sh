6984d8514c1635394943d0de00a5fc26643c9f9a
Shiva Rajendran Viji (Trianz) <shivarajendran.viji@trianz.com>
2025-03-25 23:41:10 +0530

    trying out server key

--- build/setup-prod.sh
+++ build/setup-prod.sh
@@ -1,7 +1,9 @@
 echo "Client ID: $SFDC_PROD_CLIENTID"
 echo "Username: $SFDC_PROD_USER"
 echo "server key test : $SFDC_SERVER_KEY"
-
-sfdx force:auth:jwt:grant -i "$SFDC_PROD_CLIENTID" -f "$SFDC_SERVER_KEY" -o "$SFDC_PROD_USER" --set-default-dev-hub -a devhub3
-
+echo "Key Path: '${SFDC_SERVER_KEY}'"
+mkdir keys
+chmod +w keys
+echo $SFDC_SERVER_KEY > keys/servernew.key
+sfdx force:auth:jwt:grant -i "$SFDC_PROD_CLIENTID" -f keys/servernew.key -o "$SFDC_PROD_USER" --set-default-dev-hub -a devhub3
 echo "Authentication successful!"
