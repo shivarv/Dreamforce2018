2aab5312ff7ba368115a7f35e114dea56396d4ca
Shiva Rajendran Viji (Trianz) <shivarajendran.viji@trianz.com>
2025-03-26 01:34:59 +0530

    timeout error fix

--- build/deploy-prod.sh
+++ build/deploy-prod.sh
@@ -17,4 +17,4 @@ ls -la deploy_prod
 
 # Deploy to production & run all tests
 echo "Deploying to production & running all tests..."
-sfdx project deploy start --target-org $SFDC_PROD_USER --metadata-dir deploy_prod --test-level RunAllTestsInOrg --wait 5
+sfdx project deploy start --target-org $SFDC_PROD_USER --metadata-dir deploy_prod --test-level RunAllTestsInOrg --wait 25
