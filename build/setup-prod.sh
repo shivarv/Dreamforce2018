b54e1070b6e79f0d9fd3b113c28c81dc16a5999d
Shiva Rajendran Viji (Trianz) <shivarajendran.viji@trianz.com>
2025-03-25 23:26:43 +0530

    testing by copying without enter

--- build/setup-prod.sh
+++ build/setup-prod.sh
@@ -1,6 +1,6 @@
 echo "Client ID: $SFDC_PROD_CLIENTID"
 echo "Username: $SFDC_PROD_USER"
-echo "server key : $SFDC_SERVER_KEY"
+echo "server key test : $SFDC_SERVER_KEY"
 
 sfdx force:auth:jwt:grant -i "$SFDC_PROD_CLIENTID" -f "$SFDC_SERVER_KEY" -o "$SFDC_PROD_USER" --set-default-dev-hub -a devhub3
 
