2f216e8bdd7ca2679fb241f0bf66249eb172d70e
Shiva Rajendran Viji (Trianz) <shivarajendran.viji@trianz.com>
2025-03-25 23:59:11 +0530

    reRun with copied server key

--- build/setup-prod.sh
+++ build/setup-prod.sh
@@ -1,6 +1,6 @@
 echo "Client ID: $SFDC_PROD_CLIENTID"
 echo "Username: $SFDC_PROD_USER"
-echo "server key test : $SFDC_SERVER_KEY"
+echo "server key test1 : $SFDC_SERVER_KEY"
 mkdir -p keys
 chmod +w keys
 echo $SFDC_SERVER_KEY > keys/servernew.key
