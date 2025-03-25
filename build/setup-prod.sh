4aa0c1a20edcfd1c4bc4f89e3e98f5b559b74f77
Shiva Rajendran Viji (Trianz) <shivarajendran.viji@trianz.com>
2025-03-25 23:45:30 +0530

    keys creation error

--- build/setup-prod.sh
+++ build/setup-prod.sh
@@ -2,7 +2,7 @@ echo "Client ID: $SFDC_PROD_CLIENTID"
 echo "Username: $SFDC_PROD_USER"
 echo "server key test : $SFDC_SERVER_KEY"
 echo "Key Path: '${SFDC_SERVER_KEY}'"
-mkdir keys
+mkdir -p keys
 chmod +w keys
 echo $SFDC_SERVER_KEY > keys/servernew.key
 sfdx force:auth:jwt:grant -i "$SFDC_PROD_CLIENTID" -f keys/servernew.key -o "$SFDC_PROD_USER" --set-default-dev-hub -a devhub3
