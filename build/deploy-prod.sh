61da2cb39196f6a01b74d6eefe9288f445f823b0
Shiva Rajendran Viji (Trianz) <shivarajendran.viji@trianz.com>
2025-03-26 01:09:23 +0530

    updated deploy

--- build/deploy-prod.sh
+++ build/deploy-prod.sh
@@ -1,6 +1,20 @@
+echo "Making Dir with permission."
+
+mkdir -p deploy_prod
+# Ensure it has the right permissions
+chmod -R 755 deploy_prod
 #Convert to MDAPI format for deployment to prod
 echo "Converting to MDAPI format..."
 sfdx project convert source --output-dir deploy_prod
-#Deploy to prod & run all tests
+
+# Ensure correct permissions
+echo "Setting permissions for deploy_prod..."
+chmod -R 755 deploy_prod
+
+# Verify that the conversion was successful
+echo "Checking contents of deploy_prod..."
+ls -la deploy_prod
+
+# Deploy to production & run all tests
 echo "Deploying to production & running all tests..."
 sfdx project deploy start -u $SFDC_PROD_USER --metadata-dir deploy_prod --test-level RunAllTestsInOrg --wait 5
