2ad9e7621ce1e2f417c508103f43a4bfaff2e584
Shiva Rajendran Viji (Trianz) <shivarajendran.viji@trianz.com>
2025-03-26 01:01:22 +0530

    added code

--- build/deploy-prod.sh
+++ build/deploy-prod.sh
@@ -1,6 +1,6 @@
 #Convert to MDAPI format for deployment to prod
 echo "Converting to MDAPI format..."
-sfdx force:source:convert -d deploy_prod -r force-app 
+sfdx project convert source --output-dir deploy_prod
 #Deploy to prod & run all tests
 echo "Deploying to production & running all tests..."
-sfdx force:mdapi:deploy -u devhub3 -d deploy_prod/ -w -1 -l RunAllTestsInOrg
+sfdx force:mdapi:deploy -u $SFDC_PROD_USER -d deploy_prod/ -w 5 -l RunAllTestsInOrg
