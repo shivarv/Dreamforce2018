8475ef3d22662ac9d0f22af330ccf1f42a779327
Shiva Rajendran Viji (Trianz) <shivarajendran.viji@trianz.com>
2025-03-25 17:46:48 +0530

    test

--- build/setup-prod.sh
+++ build/setup-prod.sh
@@ -1,8 +1,18 @@
 # Get the private key from the environment variable
 echo "Setting up Production Connection..."
 mkdir -p keys  # Create keys directory if it doesn't exist
-echo "$SFDC_SERVER_KEY"
+echo "SFDC_SERVER_KEY"
 echo "$SFDC_SERVER_KEY" | base64 --decode > keys/server.key
+echo "SFDC_SERVER_KEY1"
+
+echo "$SFDC_SERVER_KEY" | base64 --decode | openssl rsa -check
+echo "SFDC_SERVER_KEY2"
+
+echo "$SFDC_SERVER_KEY" | tr -d '\n' | base64 --decode > keys/server.key
+echo "SFDC_SERVER_KEY3"
+
+printf "%s" "$SFDC_SERVER_KEY" | base64 --decode > keys/server.key
+
 chmod 600 keys/server.key
 
 # Authenticate to salesforce
