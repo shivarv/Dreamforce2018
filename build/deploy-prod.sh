e5b27f57f3ea3f943ba2110c6ca0846afa30db9b
Shiva Rajendran Viji (Trianz) <shivarajendran.viji@trianz.com>
2025-03-26 02:05:06 +0530

    target org typo

--- build/deploy-prod.sh
+++ build/deploy-prod.sh
@@ -17,4 +17,4 @@ ls -la deploy_prod
 
 # Deploy to production & run all tests
 echo "Deploying to production & running all tests..."
-sfdx project deploy start --target-org $SFDC_PROD_USER --metadata-dir deploy_prod --test-level RunLocalTests  --wait 25
+sfdx project deploy start --target-org prodOrg --metadata-dir deploy_prod --test-level RunLocalTests  --wait 25
