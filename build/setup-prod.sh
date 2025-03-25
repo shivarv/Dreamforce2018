93238457da42b087141e00be6572b6673847ef86
Shiva Rajendran Viji (Trianz) <shivarajendran.viji@trianz.com>
2025-03-25 23:12:06 +0530

    added item

--- build/setup-prod.sh
+++ build/setup-prod.sh
@@ -1,5 +1,7 @@
 echo "Client ID: $SFDC_PROD_CLIENTID"
 echo "Username: $SFDC_PROD_USER"
-sfdx force:auth:jwt:grant -i "$SFDC_PROD_CLIENTID" -f keys/server.key -o "$SFDC_PROD_USER" --set-default-dev-hub -a devhub3
+echo "Username: $SFDC_SERVER_KEY"
+
+sfdx force:auth:jwt:grant -i "$SFDC_PROD_CLIENTID" -f "$SFDC_SERVER_KEY" -o "$SFDC_PROD_USER" --set-default-dev-hub -a devhub3
 
 echo "Authentication successful!"
