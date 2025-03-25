8713ca6694aad280f29759760e140c5c6333eea3
Shiva Rajendran Viji (Trianz) <shivarajendran.viji@trianz.com>
2025-03-25 22:55:24 +0530

    used variables

--- build/setup-prod.sh
+++ build/setup-prod.sh
@@ -1,3 +1,3 @@
-sfdx force:auth:jwt:grant -i "3MVG9rZjd7MXFdLgjCp0DDU4NSgi6gfX5tMCdJHCAIY17GvwgnEG8WucWSwjy9XKlRqcEbmNZN1P7YUiunURk" -f keys/server.key -u "shivarv1993867@agentforce.com" --set-default-dev-hub -a devhub3
+sfdx force:auth:jwt:grant -i "$SFDC_CLIENT_ID" -f keys/server.key -u "$SFDC_USERNAME" --set-default-dev-hub -a devhub3
 
 echo "Authentication successful!"
